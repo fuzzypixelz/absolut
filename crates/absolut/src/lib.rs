de41021ce44cb0c3794104eaa2d5be83b5fcb985
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2024-03-09 13:59:33 +0100

    test: Port tests to x86_64

--- crates/absolut/src/lib.rs
+++ crates/absolut/src/lib.rs
@@ -11,92 +11,127 @@ pub trait SimdTable<const LANES: usize> {
 
 #[cfg(test)]
 mod tests {
-    // FIXME(fuzzypixelz): add x86_64 tests
-
-    use super::{absolut, SimdTable};
-
-    #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
-    fn lookup_one_hot<const LANES: usize, T: SimdTable<LANES>>(input: &[u8; LANES]) -> [u8; LANES] {
-        use std::arch::aarch64::{
-            vandq_u8, vdupq_n_u8, vld1q_u8, vqtbl1q_u8, vshrq_n_u8, vst1q_u8,
-        };
-
-        unsafe {
-            let v_input = vld1q_u8(input.as_ptr());
-
-            let v_table_lo = vld1q_u8(T::LO.as_ptr());
-            let v_table_hi = vld1q_u8(T::HI.as_ptr());
-
-            let v_input_lo = vandq_u8(v_input, vdupq_n_u8(0b1111));
-            let v_input_hi = vshrq_n_u8::<4>(v_input);
-
-            let v_lookup_lo = vqtbl1q_u8(v_table_lo, v_input_lo);
-            let v_lookup_hi = vqtbl1q_u8(v_table_hi, v_input_hi);
-
-            let v_lookup = vandq_u8(v_lookup_lo, v_lookup_hi);
-
-            let mut lookup = [0; LANES];
-            vst1q_u8(lookup.as_mut_ptr(), v_lookup);
-            lookup
-        }
-    }
-
-    #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
-    fn lookup_one_cold<const LANES: usize, T: SimdTable<LANES>>(
-        input: &[u8; LANES],
-    ) -> [u8; LANES] {
+    #[cfg(target_arch = "aarch64")]
+    #[target_feature(enable = "neon")]
+    unsafe fn lookup_neon<const ONE_HOT: bool, Table: absolut::SimdTable<16>>(
+        input: &[u8; 16],
+    ) -> [u8; 16] {
         use std::arch::aarch64::{
             vandq_u8, vdupq_n_u8, vld1q_u8, vorrq_u8, vqtbl1q_u8, vshrq_n_u8, vst1q_u8,
         };
 
-        unsafe {
-            let v_input = vld1q_u8(input.as_ptr());
+        let v_input = vld1q_u8(input.as_ptr());
 
-            let v_table_lo = vld1q_u8(T::LO.as_ptr());
-            let v_table_hi = vld1q_u8(T::HI.as_ptr());
+        let v_table_lo = vld1q_u8(Table::LO.as_ptr());
+        let v_table_hi = vld1q_u8(Table::HI.as_ptr());
 
-            let v_input_lo = vandq_u8(v_input, vdupq_n_u8(0b1111));
-            let v_input_hi = vshrq_n_u8::<4>(v_input);
+        let v_input_lo = vandq_u8(v_input, vdupq_n_u8(0b1111));
+        let v_input_hi = vshrq_n_u8::<4>(v_input);
 
-            let v_lookup_lo = vqtbl1q_u8(v_table_lo, v_input_lo);
-            let v_lookup_hi = vqtbl1q_u8(v_table_hi, v_input_hi);
+        let v_lookup_lo = vqtbl1q_u8(v_table_lo, v_input_lo);
+        let v_lookup_hi = vqtbl1q_u8(v_table_hi, v_input_hi);
 
-            let v_lookup = vorrq_u8(v_lookup_lo, v_lookup_hi);
+        let v_lookup = if ONE_HOT {
+            vandq_u8(v_lookup_lo, v_lookup_hi)
+        } else {
+            vorrq_u8(v_lookup_lo, v_lookup_hi)
+        };
 
-            let mut lookup = [0; LANES];
-            vst1q_u8(lookup.as_mut_ptr(), v_lookup);
-            lookup
+        let mut lookup = [0; 16];
+        vst1q_u8(lookup.as_mut_ptr(), v_lookup);
+        lookup
+    }
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    #[target_feature(enable = "sse2")]
+    unsafe fn lookup_ssse3<const ONE_HOT: bool, Table: absolut::SimdTable<16>>(
+        input: &[u8; 16],
+    ) -> [u8; 16] {
+        #[cfg(target_arch = "x86")]
+        use std::arch::x86 as arch;
+
+        #[cfg(target_arch = "x86_64")]
+        use std::arch::x86_64 as arch;
+
+        use arch::{
+            _mm_and_si128, _mm_loadu_si128, _mm_or_si128, _mm_set1_epi8, _mm_shuffle_epi8,
+            _mm_srli_epi32, _mm_storeu_si128,
+        };
+
+        let v_input = _mm_loadu_si128(input.as_ptr().cast());
+
+        let v_table_lo = _mm_loadu_si128(Table::LO.as_ptr().cast());
+        let v_table_hi = _mm_loadu_si128(Table::HI.as_ptr().cast());
+
+        let v_input_lo = v_input;
+        let v_input_hi = _mm_and_si128(_mm_srli_epi32::<4>(v_input), _mm_set1_epi8(0b1111111));
+
+        let v_lookup_lo = _mm_shuffle_epi8(v_table_lo, v_input_lo);
+        let v_lookup_hi = _mm_shuffle_epi8(v_table_hi, v_input_hi);
+
+        let v_lookup = if ONE_HOT {
+            _mm_and_si128(v_lookup_lo, v_lookup_hi)
+        } else {
+            // ONE_COLD
+            _mm_or_si128(v_lookup_lo, v_lookup_hi)
+        };
+
+        let mut lookup = [0; 16];
+        _mm_storeu_si128(lookup.as_mut_ptr().cast(), v_lookup);
+        lookup
+    }
+
+    fn lookup_fallback<const ONE_HOT: bool, Table: absolut::SimdTable<16>>(
+        input: &[u8; 16],
+    ) -> [u8; 16] {
+        let mut lookup = [0; 16];
+
+        for (index, byte) in input.iter().copied().enumerate() {
+            let lo = byte & 0b1111;
+            let hi = byte >> 4;
+
+            lookup[index] = if ONE_HOT {
+                Table::LO[lo as usize] & Table::HI[hi as usize]
+            } else {
+                // ONE_COLD
+                Table::LO[lo as usize] | Table::HI[hi as usize]
+            };
+        }
+
+        lookup
+    }
+
+    fn lookup<const ONE_HOT: bool, Table: absolut::SimdTable<16>>(input: &[u8; 16]) -> [u8; 16] {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        {
+            if std::arch::is_x86_feature_detected!("ssse3") {
+                unsafe { lookup_ssse3::<ONE_HOT, Table>(input) }
+            } else {
+                lookup_fallback::<ONE_HOT, Table>(input)
+            }
+        }
+        #[cfg(target_arch = "aarch64")]
+        {
+            if std::arch::is_aarch64_feature_detected!("neon") {
+                unsafe { lookup_neon::<ONE_HOT, Table>(input) }
+            } else {
+                lookup_fallback::<ONE_HOT, Table>(input)
+            }
+        }
+        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64")))]
+        {
+            lookup_fallback::<ONE_HOT, Table>(input)
         }
     }
 
-    #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
-    fn lookup_general<const LANES: usize, T: SimdTable<LANES>>(input: &[u8; LANES]) -> [u8; LANES] {
-        use std::arch::aarch64::{
-            vandq_u8, vdupq_n_u8, vld1q_u8, vqtbl1q_u8, vshrq_n_u8, vst1q_u8,
-        };
-
-        unsafe {
-            let v_input = vld1q_u8(input.as_ptr());
-
-            let v_table_lo = vld1q_u8(T::LO.as_ptr());
-            let v_table_hi = vld1q_u8(T::HI.as_ptr());
-
-            let v_input_lo = vandq_u8(v_input, vdupq_n_u8(0b1111));
-            let v_input_hi = vshrq_n_u8::<4>(v_input);
-
-            let v_lookup_lo = vqtbl1q_u8(v_table_lo, v_input_lo);
-            let v_lookup_hi = vqtbl1q_u8(v_table_hi, v_input_hi);
-
-            let v_lookup = vandq_u8(v_lookup_lo, v_lookup_hi);
-
-            let mut lookup = [0; LANES];
-            vst1q_u8(lookup.as_mut_ptr(), v_lookup);
-            lookup
-        }
+    fn lookup_one_hot<Table: absolut::SimdTable<16>>(input: &[u8; 16]) -> [u8; 16] {
+        lookup::<true, Table>(input)
+    }
+    fn lookup_one_cold<Table: absolut::SimdTable<16>>(input: &[u8; 16]) -> [u8; 16] {
+        lookup::<false, Table>(input)
     }
 
     #[test]
-    #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
     fn test_ident() {
         macro_rules! test_ident {
             ($algorithm:meta, $lookup:ident) => {{
@@ -123,7 +158,7 @@ mod tests {
 
                 use Table::*;
                 assert_eq!(
-                    $lookup::<16, Table>(&input),
+                    $lookup::<Table>(&input),
                     [
                         Underscore as u8,
                         Lowercase2 as u8,
@@ -148,12 +183,11 @@ mod tests {
 
         test_ident!(absolut::one_cold, lookup_one_cold);
         test_ident!(absolut::one_hot, lookup_one_hot);
-        test_ident!(absolut::general, lookup_general);
-        test_ident!(absolut::general(powers_of_two), lookup_general);
+        test_ident!(absolut::general, lookup_one_hot);
+        test_ident!(absolut::general(powers_of_two), lookup_one_hot);
     }
 
     #[test]
-    #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
     fn test_json() {
         macro_rules! test_json {
             ($algorithm:meta, $lookup:ident) => {{
@@ -178,7 +212,7 @@ mod tests {
 
                 use Table::*;
                 assert_eq!(
-                    $lookup::<16, Table>(&input),
+                    $lookup::<Table>(&input),
                     [
                         Other as u8,
                         Other as u8,
@@ -203,7 +237,7 @@ mod tests {
 
         test_json!(absolut::one_cold, lookup_one_cold);
         test_json!(absolut::one_hot, lookup_one_hot);
-        test_json!(absolut::general, lookup_general);
-        test_json!(absolut::general(powers_of_two), lookup_general);
+        test_json!(absolut::general, lookup_one_hot);
+        test_json!(absolut::general(powers_of_two), lookup_one_hot);
     }
 }
